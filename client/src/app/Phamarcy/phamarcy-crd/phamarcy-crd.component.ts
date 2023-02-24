b8eab8a3a92b323d147886d499728c6a5dcbedc8
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2023-02-24 14:58:57 +0530

    phamaracy crd and view

---
+++ client/src/app/Phamarcy/phamarcy-crd/phamarcy-crd.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Phmaracy } from 'src/app/models/phmaracy.model';
+@Component({
+  selector: 'app-phamarcy-crd',
+  templateUrl: './phamarcy-crd.component.html',
+  styleUrls: ['./phamarcy-crd.component.scss']
+})
+export class PhamarcyCrdComponent implements OnInit {
+  @Input() phamarcy!: Phmaracy;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
